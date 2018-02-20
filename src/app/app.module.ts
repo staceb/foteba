b7a2333ed789376b7773b3deb1c63a95695a944d
Kenneth Rodolfo Becker Carletto <chorao.info@gmail.com>
2018-02-19 23:23:07 -0300

    Navbar e melhoria visual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { MaterializeModule } from 'angular2-materialize';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './component/login/login.component';
+import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    
+    AppComponent, LoginComponent, MenuComponent
   ],
   imports: [
     BrowserModule
